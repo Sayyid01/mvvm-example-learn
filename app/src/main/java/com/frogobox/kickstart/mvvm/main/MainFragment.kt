aa712d749ce9faa72f55941189056d06ac0ca1d6
Sayyid F <42909243+Sayyid01@users.noreply.github.com>
2021-04-06 15:46:38 +0700

    Initial commit

---
+++ app/src/main/java/com/frogobox/kickstart/mvvm/main/MainFragment.kt
@@ -0,0 +1,23 @@
+package com.frogobox.kickstart.mvvm.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.frogobox.kickstart.R
+import com.frogobox.kickstart.base.view.ui.BaseFragment
+
+class MainFragment : BaseFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+
+}
